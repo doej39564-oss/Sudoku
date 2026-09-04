8eb77043952621a9d550e58f4ecd377afa7ac2bc
doej39564-oss <doej39564@gmail.com>
2026-09-04 13:01:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const C="sudoku-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const C="sudoku-v2";
+self.addEventListener("install",e=>e.waitUntil(
+  caches.open(C).then(c=>c.addAll([
+    "./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"
+  ]))
+));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
+));
+self.addEventListener("fetch",e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
